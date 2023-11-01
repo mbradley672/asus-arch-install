a3daf652c531dc94569e2bcc3c15876108f0a14d
Melvin Bradley <mbradley672@gmail.com>
2023-10-31 23:12:29 -0400

    Create asus-arch-install.sh

---
+++ asus-arch-install.sh
@@ -0,0 +1,8 @@
+pacman-key --recv-keys 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+pacman-key --lsign-key 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+
+wget "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x8b15a6b0e9a3fa35" -O g14.sec
+sudo pacman-key -a g14.sec
+
