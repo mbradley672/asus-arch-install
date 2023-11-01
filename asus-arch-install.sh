8dc64b473bb50e0c0fb392b463b0b5fede6832bd
Melvin Bradley <mbradley672@gmail.com>
2023-10-31 23:17:49 -0400

    Update asus-arch-install.sh

--- asus-arch-install.sh
+++ asus-arch-install.sh
@@ -6,3 +6,12 @@ pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
 wget "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x8b15a6b0e9a3fa35" -O g14.sec
 sudo pacman-key -a g14.sec
 
+sudo cat g14-repo-conf >> /etc/pacman.conf
+
+pacman -Suy
+
+pacman -S asusctl
+systemctl enable --now power-profiles-daemon.service
+pacman -S supergfxctl
+systemctl enable --now supergfxd
+pacman -S rog-control-center
